37fb9016f9f2fa91c8d7199a35bfab8845e60394
kayhanayar <kayar@sakarya.edu.tr>
2018-07-19 11:29:19 +0300

    Yeni

--- Ders-03/src/main.cpp
+++ Ders-03/src/main.cpp
@@ -7,6 +7,4 @@ int main(int argc,char** argv)
 {
 
     std::cout<<topla(10,5)<<std::endl;
-    
-    std::cin.get();
 }
--- Ders-04/src/main.cpp
+++ Ders-04/src/main.cpp
@@ -7,4 +7,6 @@ int main(int argc,char** argv)
 {
 
     std::cout<<topla(10,5)<<std::endl;
+    
+    std::cin.get();
 }
